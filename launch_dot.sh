ff2916c6742ba0b2a3503022ab110962b533ae5c
k9i <k9i@users.noreply.github.com>
2021-06-05 14:43:31 +0900

    Add git credential.helper before git clone

--- launch_dot.sh
+++ launch_dot.sh
@@ -3,9 +3,10 @@ set -Eeuo pipefail
 
 cd $HOME
 pwd
+( set -x; git config --global credential.helper || git config --global --replace-all credential.helper 'cache --timeout 2592000' )
 
 if [[ ! -d .dot ]]; then
-  git clone https://k9i@github.com/k9i/dotfile.git .dot
+  ( set -x; git clone https://k9i@github.com/k9i/dotfile.git .dot )
 fi
 
 exec .dot/bin/setup_dot.sh
