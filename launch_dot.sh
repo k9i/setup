38288888474e9f1580c57b61de655122d2428f42
k9i <k9i@users.noreply.github.com>
2021-08-15 13:57:42 +0900

    Use `--depth 1` to clone

--- launch_dot.sh
+++ launch_dot.sh
@@ -6,7 +6,7 @@ pwd
 ( set -x; git config --global credential.helper || git config --global --replace-all credential.helper 'cache --timeout 2592000' )
 
 if [[ ! -d .dot ]]; then
-  ( set -x; git clone https://k9i@github.com/k9i/dotfile.git .dot )
+  ( set -x; git clone --depth 1 https://k9i@github.com/k9i/dotfile.git .dot )
 fi
 
 exec .dot/bin/setup_dot.sh
